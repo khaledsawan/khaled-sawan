833032d562a627ef0437254206092ff48c35328c
khaledsawan <aky2115327@gmail.com>
2025-12-03 07:54:30 +0300

    Refactor code structure for improved readability and maintainability

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "677690107"
+    serviceWorkerVersion: "2121566960"
   }
 });
