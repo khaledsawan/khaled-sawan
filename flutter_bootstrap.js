f5829909c7e6fdff4947f0c847eb81a6c18dcd24
khaledsawan <aky2115327@gmail.com>
2025-12-04 00:21:49 +0300

    Update project name and related configurations
    - Changed application name from "myportfolio" to "khaledsawan" in manifest.json and version.json.
    - Updated short name in manifest.json to reflect the new application name.
    - Adjusted package name in version.json to match the new branding.
    - Binary file main.dart.wasm has been updated.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2121566960"
+    serviceWorkerVersion: "2332840032"
   }
 });
